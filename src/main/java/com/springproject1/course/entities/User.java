2fa75aafd4398f5e920471ac844d1343b03beb58
Lucas <lucas.desa@outlook.com.br>
2023-01-18 11:04:49 -0300

    OrderStatus Enum

--- src/main/java/com/springproject1/course/entities/User.java
+++ src/main/java/com/springproject1/course/entities/User.java
@@ -18,7 +18,7 @@ import jakarta.persistence.Table;
 @Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,11 +26,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-		
+
 	public User() {
 	}
 
@@ -81,11 +81,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public List<Order> getOrders() {
 		return orders;
-	}	
-	
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
